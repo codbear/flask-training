52f1f04f7836b8a934d34c88369c6a46c6d1b9c6
codbear <douglas.meunier@openclassrooms.com>
2021-11-08 23:18:29 +0100

    build(flask): initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# To generate a new secret key:
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+SECRET_KEY = "0l@oEP~]gc`Z~)[1(w=Xgp%u"
+
+FB_APP_ID = 1200420960103822
